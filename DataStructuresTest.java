4fd1aa14c07724989f8d71be63092860ebb91444
Udara Neminda <udaraneminda@gmail.com>
2024-10-20 12:56:41 +0530

    fisrt commit

---
+++ DataStructuresTest.java
@@ -0,0 +1,19 @@
+package datastructurestest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class DataStructuresTest {
+    
+    public static ArrayList<String> AL = new ArrayList<>();
+    public static HashMap<String, Integer> HM = new HashMap<>();
+    public static LinkedList<String> LL = new LinkedList<>();
+
+    public static void main(String[] args) {
+        AL.add("Array List Test");
+        HM.put("Hash Map Test",0);
+        LL.push("Linked List Test");
+    }
+    
+}
